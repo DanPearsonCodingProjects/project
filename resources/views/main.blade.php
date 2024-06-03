2b2bc5c78dabd4d49634b7999629f841355b430f
Daniel Pearson <danpearson1701@gmail.com>
2024-06-03 21:17:58 +0100

    Layout changes for main

---
+++ resources/views/main.blade.php
@@ -0,0 +1,11 @@
+@extends('Layouts.layoutMain')
+
+@section('content')
+
+<div class="login">
+    <form action="">
+        <input type="text" placeholder="Enter Login">
+        <input type="password" placeholder="Enter Password">
+    </form>
+</div>
+@endsection
